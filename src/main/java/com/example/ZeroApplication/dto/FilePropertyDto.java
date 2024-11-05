132bde334636e4b700d799db9f2638b517194a15
sunnyst4r <ivan_ivanov_13.02@mail.ru>
2024-11-06 00:23:00 +0300

    fix pom

--- src/main/java/com/example/ZeroApplication/dto/FilePropertyDto.java
+++ src/main/java/com/example/ZeroApplication/dto/FilePropertyDto.java
@@ -1,6 +1,5 @@
 package com.example.ZeroApplication.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,7 +7,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class FilePropertyDto {
     String url;
 }
