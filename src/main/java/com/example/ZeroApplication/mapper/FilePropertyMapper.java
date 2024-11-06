3264c665aea87cae1128b7922827b3d6d0ba405a
sunnyst4r <ivan_ivanov_13.02@mail.ru>
2024-11-06 19:11:33 +0300

    fix

--- src/main/java/com/example/ZeroApplication/mapper/FilePropertyMapper.java
+++ src/main/java/com/example/ZeroApplication/mapper/FilePropertyMapper.java
@@ -3,20 +3,12 @@ package com.example.ZeroApplication.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
-
-import com.example.ZeroApplication.config.Config;
 import com.example.ZeroApplication.dto.FilePropertyDto;
 import com.example.ZeroApplication.entity.FilePropertyEntity;
 
 @Mapper(componentModel = "spring")
 public abstract class FilePropertyMapper {
-    @Mapping(target = "link", source = "uuid", qualifiedByName = "toUrl")
+    @Mapping(target = "link", source = "uuid")
     public abstract FilePropertyDto toFilePropertyDto(FilePropertyEntity file);
-
-    @Named("toUrl")
-    public static String toUrl(String uuid) {
-        return Config.PATH.toString() + uuid;
-    }
 }
 
